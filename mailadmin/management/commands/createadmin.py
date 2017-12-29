707f3af2c5e9edb9103fce4e918232b1dab024df
Andreas Elvers <andreas@work.de>
2017-12-29 16:55:00 +0100

    adding username and password options

--- mailadmin/management/commands/createadmin.py
+++ mailadmin/management/commands/createadmin.py
@@ -8,28 +8,35 @@ from mailadmin.models import UserAdditions
 
 
 class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('--username', default=None)
+        parser.add_argument('--password', default=None)
+
     def handle(self, *args, **options):
-        try:
-            self.stdout.write('Create admin user for you MailAdmin installation:')
-            default_user_name = 'admin'
-            username = str(input('Username [%s]: ' % default_user_name)).strip() or default_user_name
-            password = str(getpass('Password: '))
-
+        username = options['username']
+        password = options['password']
+        if not (username or password):
             try:
-                User.objects.get(username__exact=username)
-                self.stdout.write(self.style.ERROR('User with name %s does already exist.' % username))
-            except ObjectDoesNotExist:
-                user = User()
-                user.username = username
-                user.set_password(password)
-                user.save()
+                self.stdout.write('Create admin user for you MailAdmin installation:')
+                default_user_name = 'admin'
+                username = str(input('Username [%s]: ' % default_user_name)).strip() or default_user_name
+                password = str(getpass('Password: '))
+            except (KeyboardInterrupt, EOFError):
+                print()
+                return
+        try:
+            User.objects.get(username__exact=username)
+            self.stdout.write(self.style.ERROR('User with name %s does already exist.' % username))
+        except ObjectDoesNotExist:
+            user = User()
+            user.username = username
+            user.set_password(password)
+            user.save()
 
-                useradditions = UserAdditions()
-                useradditions.user = user
-                useradditions.roles = 4
-                useradditions.save()
+            useradditions = UserAdditions()
+            useradditions.user = user
+            useradditions.roles = 4
+            useradditions.save()
 
-                self.stdout.write(self.style.SUCCESS('Admin user %s created.' % username))
-        except (KeyboardInterrupt, EOFError):
-            print()
-            pass
+            self.stdout.write(self.style.SUCCESS('Admin user %s created.' % username))
